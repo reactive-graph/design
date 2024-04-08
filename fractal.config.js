3cf053e57074fbfe475946a6e33e4153a2fa1042
Andreas Schaeffer <andreas@schaeffernet.de>
2024-04-08 22:59:35 +0200

    Configure build destination

--- fractal.config.js
+++ fractal.config.js
@@ -9,3 +9,4 @@ fractal.set('project.version', '0.1.0');
 fractal.components.set('path', path.join(__dirname, 'components'));
 fractal.docs.set('path', path.join(__dirname, 'docs'));
 fractal.web.set('static.path', path.join(__dirname, 'public'));
+fractal.web.set('builder.dest', path.join(__dirname, 'build'));
