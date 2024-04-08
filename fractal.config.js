734cca3d6293d162d139ab96c69d9d65e284c385
Andreas Schaeffer <andreas@schaeffernet.de>
2024-04-08 22:58:31 +0200

    Set version and cleanup

--- fractal.config.js
+++ fractal.config.js
@@ -1,31 +1,11 @@
 'use strict';
 
-/*
-* Require the path module
-*/
 const path = require('path');
-
-/*
- * Require the Fractal module
- */
 const fractal = module.exports = require('@frctl/fractal').create();
 
-/*
- * Give your project a title.
- */
 fractal.set('project.title', 'Reactive Graph - Design System');
+fractal.set('project.version', '0.1.0');
 
-/*
- * Tell Fractal where to look for components.
- */
 fractal.components.set('path', path.join(__dirname, 'components'));
-
-/*
- * Tell Fractal where to look for documentation pages.
- */
 fractal.docs.set('path', path.join(__dirname, 'docs'));
-
-/*
- * Tell the Fractal web preview plugin where to look for static assets.
- */
 fractal.web.set('static.path', path.join(__dirname, 'public'));
