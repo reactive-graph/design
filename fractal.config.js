da6e4f1c1e0bb372839c71b4988f6566de37d8f4
Andreas Schaeffer <andreas@schaeffernet.de>
2024-04-09 22:46:58 +0200

    Adjust fractal config

--- fractal.config.js
+++ fractal.config.js
@@ -7,6 +7,26 @@ fractal.set('project.title', 'Reactive Graph - Design System');
 fractal.set('project.version', '0.1.0');
 
 fractal.components.set('path', path.join(__dirname, 'components'));
+fractal.components.set('ext', '.hbs');
+fractal.components.set('default.status', 'wip');
+fractal.components.set('default.preview', '@preview');
+
 fractal.docs.set('path', path.join(__dirname, 'docs'));
+fractal.docs.set('ext', '.md');
+fractal.docs.set('default.status', 'draft');
+const hbs = require('@frctl/handlebars')({
+  helpers: {
+    componentList: function() {
+      let ret = "<ul>";
+      const options = Array.from(arguments).pop();
+      for (let component of fractal.components.flatten()) {
+        ret = ret + "<li>" + options.fn(component.toJSON()) + "</li>";
+      }
+      return ret + "</ul>";
+    }
+  }
+});
+fractal.docs.engine(hbs);
+
 fractal.web.set('static.path', path.join(__dirname, 'public'));
 fractal.web.set('builder.dest', path.join(__dirname, 'build'));
