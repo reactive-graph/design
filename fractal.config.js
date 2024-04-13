0c47840958b4f504160d7a9255733572abb6df84
Andreas Schaeffer <andreas@schaeffernet.de>
2024-04-14 00:21:17 +0200

    Specify fractal statuses for components and documentation pages + Specify the ordering of the navigation + Externalize css

--- fractal.config.js
+++ fractal.config.js
@@ -3,18 +3,63 @@
 const path = require('path');
 const fractal = module.exports = require('@frctl/fractal').create();
 const mandelbrot = require('@frctl/mandelbrot');
+const pkg = require(path.join(__dirname, 'package.json'));
 
 fractal.set('project.title', 'Reactive Graph - Design System');
-fractal.set('project.version', '0.1.0');
+fractal.set('project.version', pkg.version);
 
 fractal.components.set('path', path.join(__dirname, 'components'));
 fractal.components.set('ext', '.hbs');
-fractal.components.set('default.status', 'wip');
+fractal.components.set('default.status', 'prototype');
 fractal.components.set('default.preview', '@preview');
+fractal.components.set('statuses', {
+  prototype: {
+    label: "Prototype",
+    description: "Do not implement.",
+    color: "#dc0073"
+  },
+  wip: {
+    label: "WIP",
+    description: "Work in progress. Implement with caution.",
+    color: "#f5b700"
+  },
+  ready: {
+    label: "Ready",
+    description: "Ready to implement.",
+    color: "#04e762"
+  },
+  deprecated: {
+    label: "Deprecated",
+    description: "Deprecated component slated for removal.",
+    color: "firebrick"
+  },
+})
 
 fractal.docs.set('path', path.join(__dirname, 'docs'));
 fractal.docs.set('ext', '.md');
-fractal.docs.set('default.status', 'draft');
+fractal.docs.set('statuses', {
+  alpha: {
+    label: "Alpha",
+    description: "This documentation is a draft.",
+    color: "#dc0073"
+  },
+  beta: {
+    label: "Beta",
+    description: "Work in progress. The documentation is actively being written but not ready yet.",
+    color: "#f5b700"
+  },
+  stable: {
+    label: "Stable",
+    description: "The documentation is stable.",
+    color: "#04e762"
+  },
+  outdated: {
+    label: "Outdated",
+    description: "Outdated documentation, needs updating.",
+    color: "firebrick"
+  }
+});
+fractal.docs.set('default.status', 'alpha');
 const hbs = require('@frctl/handlebars')({
     helpers: {
         componentList: function () {
@@ -31,14 +76,13 @@ fractal.docs.engine(hbs);
 
 fractal.web.set('static.path', path.join(__dirname, 'public'));
 fractal.web.set('builder.dest', path.join(__dirname, 'build'));
+fractal.web.set('server.sync', true);
 
 const customTheme = mandelbrot({
         styles: [
             'default',
-            '/css/open-props.min.css',
-            '/css/open-props-normalize.min.css',
-            '/css/reactive-graph.css',
-            '/css/fractal.css'
+            '/css/fractal.css',
+            '/css/reactive-graph.css'
         ],
         favicon: '/favicon/favicon.svg',
         skin: {
@@ -46,10 +90,8 @@ const customTheme = mandelbrot({
             accent: '#00a1e4',
             complement: '#dc0073',
             links: '#00a1e4',
-            // accent: '#dc0073',
-            // complement: '#89fc00',
-            // links: '#00a1e4',
         },
+        nav: ['search', 'docs', 'components'],
         navigation: 'split',
         information: [
             {
