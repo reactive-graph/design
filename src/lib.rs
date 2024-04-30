7e6313239d27161b9d93615fffe9d265745f6540
Andreas Schaeffer <andreas@schaeffernet.de>
2024-04-30 22:48:53 +0200

    Implemented token as web components

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 
-use crate::atoms::tag::RgTag;
-use crate::base::CustomElementCreator;
-
 pub mod atoms;
 pub mod molecules;
 pub mod organisms;
@@ -11,5 +8,5 @@ pub mod base;
 
 #[wasm_bindgen(start)]
 fn run() {
-  RgTag::define_element();
+  atoms::define_elements();
 }
