79fcba84e5c224c9dd9bdf07b6fb098af3e85a45
Andreas Schaeffer <andreas@schaeffernet.de>
2024-10-27 13:20:13 +0100

    Make GraphQL voyager work with a schema url

--- public/js/graphql-voyager/init.js
+++ public/js/graphql-voyager/init.js
@@ -2,27 +2,39 @@
 document.addEventListener('DOMContentLoaded', (_event) => {
   document.querySelectorAll('div.graphql-voyager-wrapper').forEach(async (element) => {
     const endpoint = element.getAttribute('data-endpoint');
-    if (!endpoint) {
-      element.innerHTML = 'Missing endpoint URL. <code>data-endpoint</code>';
+    const schema = element.getAttribute('data-schema');
+    if (!endpoint && !schema) {
+      element.innerHTML = 'Missing GraphQL endpoint URL or schema URL. Use either <code>data-endpoint="url"</code> <code>data-schema="url"</code>';
       return;
+    }
+    var introspection;
+    if (schema != null) {
+      console.log(`Loading schema from ${schema}`);
+      const response = await fetch(schema);
+      let sdl = await response.text();
+      introspection = {
+        "data": introspectionFromSchema(buildSchema(sdl))
+      };
+
+    } else if (endpoint != null) {
+      console.log(`Loading schema by introspection of GraphQL endpoint ${endpoint}`);
+      const {voyagerIntrospectionQuery: query} = GraphQLVoyager;
+
+      const response = await fetch(
+        endpoint,
+        {
+          method: 'post',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({query}),
+        },
+      );
+      introspection = await response.json();
     }
     const hideDocs = element.getAttribute('data-hide-docs') === "true";
     const hideSettings = element.getAttribute('data-hide-settings') === "true";
-    const {voyagerIntrospectionQuery: query} = GraphQLVoyager;
-
-    const response = await fetch(
-      endpoint,
-      {
-        method: 'post',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({query}),
-        credentials: 'omit',
-      },
-    );
-    const introspection = await response.json();
     GraphQLVoyager.renderVoyager(element, {
       introspection,
       hideDocs,
